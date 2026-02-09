4bc9f65271c343253be3e2fb2947468655119e6d
DeceptiveGD <nicjoar@icloud.com>
2026-02-09 19:17:14 +0200

    it didnt woprk

--- main/index.js
+++ main/index.js
@@ -5,7 +5,7 @@ function firstPage()
     let htmlHeader = "";
     var body = document.getElementById("container");
 
-    fetch("./framework/header.html")
+    fetch("./main/framework/header.html")
     .then(response => {
     return response.text();
     })
