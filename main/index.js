47a4dcd4d4e7a79e3da0a5ebfa3b28ebab635d7c
DeceptiveGD <nicjoar@icloud.com>
2026-02-10 14:39:19 +0200

    first page work

--- main/index.js
+++ main/index.js
@@ -1,16 +1,33 @@
+let htmlBodyCode = "";
 
-function firstPage()
+function mainPage()
 {
     console.log("javascript")
-    let htmlHeader = "";
-    var body = document.getElementById("container");
+    let htmlCode = "";
+    var body = document.getElementById("lefttext");
 
-    fetch("./main/framework/header.html")
+    //gets html text from header file
+    fetch("./main/texts/lefttext")
     .then(response => {
     return response.text();
     })
     .then(htmlText => {
-        htmlHeader = htmlText;
-        body.innerHTML = htmlHeader;
+        htmlCode = htmlText;
+        htmlBodyCode += htmlCode;
+        //writes the fetched html code into index.html
+        body.innerHTML = htmlCode;
+    });
+
+    var body2 = document.getElementById("righttext");
+
+    fetch("./main/texts/righttext")
+    .then(response => {
+    return response.text();
+    })
+    .then(htmlText => {
+        htmlCode = htmlText;
+        htmlBodyCode += htmlCode;
+        //writes the fetched html code into index.html
+        body2.innerHTML = htmlCode;
     });
 }
